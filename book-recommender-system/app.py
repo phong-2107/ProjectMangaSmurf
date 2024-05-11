7f2cc8d22ed3578f6b1eb7ae983bb8a42181f2bb
Karry_Pham <153251748+Linhdan3113@users.noreply.github.com>
2024-05-12 02:49:16 +0700

    [UPDATE] API goi y sach
    - request: gửi mã sách
    - response: nhận 5 sách gợi ý

--- book-recommender-system/app.py
+++ book-recommender-system/app.py
@@ -15,7 +15,6 @@ class NpEncoder(json.JSONEncoder):
 
 
 popular_df = pickle.load(open('popular.pkl','rb'))
-
 pt = pickle.load(open('pt.pkl','rb'))
 books = pickle.load(open('books.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
@@ -27,7 +26,6 @@ def index():
     return render_template('index.html',
                            book_name = list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
-                           image=list(popular_df['Image-URL-M'].values),
                            votes=list(popular_df['num_ratings'].values),
                            rating=list(popular_df['avg_rating'].values)
                            )
@@ -45,6 +43,9 @@ def recommend():
     data = []
     for i in similar_items:
         item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
 
         data.append(item)
 
@@ -52,4 +53,91 @@ def recommend():
 
     return render_template('recommend.html',data=data)
 
+# API goi y sach
+@app.route('/goi-y-sach', methods=['POST'])
+def goi_y_sach():
+    # Kiểm tra xem request có phải là POST không
+    if request.method == 'POST':
+
+        if request.is_json:
+            data = request.get_json()
+            # print(data)
+            # Kiểm tra xem key 'book-id' có trong dữ liệu không
+            if 'Book-Id' in data:
+                book_id = data['Book-Id']
+                try:
+                    recommended_books = recommend(book_id)
+                    # print(recommended_books)
+                    recommended_books_json = [int(item[0]) for item in recommended_books]
+                    print(type(recommended_books_json))
+                    recommended_books_json = json.dumps(recommended_books_json, cls=NpEncoder)
+                    print("SAU khi: bien doi ", type(recommended_books_json))
+                    print("A: ", json.dumps(recommended_books_json))
+                    if recommended_books is not None:
+                        # Chuyển đổi recommended_books thành một đối tượng có thể chuyển đổi thành JSON
+                        # Chuyển đổi các giá trị số thành int
+                        recommended_books_json = [int(item[0]) for item in recommended_books]
+                        # recommended_books_json = json.dumps(recommended_books_json, cls=NpEncoder)
+
+                        response = {
+                            "status": 200,
+                            "recommended_books_id": recommended_books_json
+                        }
+                        return jsonify(response), 200
+                    else:
+                        response = {
+                            "status": 400,
+                            "message": "Book ID not found or error occurred"
+                        }
+                        return jsonify(response), 400
+                except Exception as e:
+                    # Bắt ngoại lệ và in ra thông tin chi tiết về lỗi
+                    print("An error occurred:", str(e))
+                    response = {
+                        "status": 400,
+                        "message": "An error occurred"
+                    }
+                    return jsonify(response), 400
+            else:
+                response = {
+                    "status": 400,
+                    "message": "'Book-Id' key not found in request"
+                }
+                return jsonify(response), 400
+        else:
+            response = {
+                "status": 400,
+                "message": "Request must be in JSON format"
+            }
+            return jsonify(response), 400
+    else:
+        response = {
+            "status": 405,
+            "message": "Method Not Allowed"
+        }
+        return jsonify(response), 405
+
+def recommend(book_id):
+    book_id = int(book_id)
+    # Kiểm tra xem book_name có tồn tại trong pt.index không
+    if book_id in pt.index:
+        # index fetch
+        index = np.where(pt.index == book_id)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+        
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Id'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Id'].values))
+            
+            data.append(item)
+        
+        return data
+    else:
+        print("Book not found in database")
+        return None
+
+
 if __name__ == '__main__':
+    app.run(debug=True)
