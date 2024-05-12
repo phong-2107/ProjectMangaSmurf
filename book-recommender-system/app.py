1ea272c4a7b1b82db59fccb4d8b39eeec5378a37
NT-Phong <nguyenphong.210703@gmail.com>
2024-05-12 17:57:38 +0700

    [Update] file pt.pkl

--- book-recommender-system/app.py
+++ book-recommender-system/app.py
@@ -73,61 +73,45 @@ def recommend():
 # API goi y sach
 @app.route('/goi-y-sach', methods=['POST'])
 def goi_y_sach():
-    # Kiểm tra xem request có phải là POST không
     if request.method == 'POST':
-
         if request.is_json:
             data = request.get_json()
-            # print(data)
-            # Kiểm tra xem key 'book-id' có trong dữ liệu không
-            if 'Book-Id' in data:
-                book_id = data['Book-Id']
+            # Sử dụng key chính xác từ JSON
+            book_id = data.get('Book-Id')
+            if book_id:
                 try:
-                    recommended_books = recommend(book_id)
-                    if recommended_books is not None:
-                        # Chuyển đổi recommended_books thành một đối tượng có thể chuyển đổi thành JSON
-                        # Chuyển đổi các giá trị số thành mảng int
+                    recommended_books = recommend(int(book_id))
+                    if recommended_books:
                         recommended_books_json = [int(item[0]) for item in recommended_books]
-                        # recommended_books_json = json.dumps(recommended_books_json, cls=NpEncoder)
-
-                        response = {
+                        return jsonify({
                             "status": 200,
                             "recommended_books_id": recommended_books_json
-                        }
-                        return jsonify(response), 200
+                        }), 200
                     else:
-                        response = {
+                        return jsonify({
                             "status": 400,
-                            "message": "Book ID not found or error occurred"
-                        }
-                        return jsonify(response), 400
+                            "message": "Book ID not found"
+                        }), 400
                 except Exception as e:
-                    # Bắt ngoại lệ và in ra thông tin chi tiết về lỗi
-                    print("An error occurred:", str(e))
-                    response = {
+                    return jsonify({
                         "status": 400,
-                        "message": "An error occurred"
-                    }
-                    return jsonify(response), 400
+                        "message": f"An error occurred: {str(e)}"
+                    }), 400
             else:
-                response = {
+                return jsonify({
                     "status": 400,
                     "message": "'Book-Id' key not found in request"
-                }
-                return jsonify(response), 400
+                }), 400
         else:
-            response = {
+            return jsonify({
                 "status": 400,
                 "message": "Request must be in JSON format"
-            }
-            return jsonify(response), 400
+            }), 400
     else:
-        response = {
+        return jsonify({
             "status": 405,
             "message": "Method Not Allowed"
-        }
-        return jsonify(response), 405
-
+        }), 405
 def recommend(book_id):
     book_id = int(book_id)
     # Kiểm tra xem book_name có tồn tại trong pt.index không
